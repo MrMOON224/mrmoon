0f3725fa468b1c6bad3b6875d089236631ac4964
MrMOON224 <90089357+MrMOON224@users.noreply.github.com>
2022-09-11 15:59:21 +0630

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-$(document).ready(function(){
-  $('#menu-icon').on('click', function(){
-    $('.navbar').toggleClass('expand');
-    return false;
-  });
-});
-function myFunction() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
+// define all UI variable
+const navToggler = document.querySelector('.nav-toggler');
+const navMenu = document.querySelector('.site-navbar ul');
+const navLinks = document.querySelectorAll('.site-navbar a');
+
+// load all event listners
+allEventListners();
+
+// functions of all event listners
+function allEventListners() {
+  // toggler icon click event
+  navToggler.addEventListener('click', togglerClick);
+  // nav links click event
+  navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
 }
-AOS.init()
+
+// togglerClick function
+function togglerClick() {
+  navToggler.classList.toggle('toggler-open');
+  navMenu.classList.toggle('open');
+}
+
+// navLinkClick function
+function navLinkClick() {
+  if(navMenu.classList.contains('open')) {
+    navToggler.click();
+  }
+}
